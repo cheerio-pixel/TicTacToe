bfcf0c42d8d3783815734fc1dfae046f320dcec4
cheerio-pixel <frailin300@gmail.com>
2021-04-07 21:19:12 -0400

    Formatting

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import numpy as np
-
 from collections import deque
+
 
 # BOARD
 class Board:
     def __init__(self):
         self.board = np.zeros((3, 3))
 
-    ## Insert
+    # Insert
     def insert(self, coords, player):
-        x -= coords[0]
-        y -= coords[1]
+        x = coords[0] - 1
+        y = coords[1] - 1
         self.board[x, y] = player
 
-    ## Display
+    # Display
     def display(self):
         for item in self.board:
             for second_item in item:
@@ -24,7 +24,7 @@ class Board:
                 if second_item == 2:
                     yield "[O]"
 
-    ## Reset
+    # Reset
     def reset(self):
         self.board = np.zeros((3, 3))
 
@@ -36,15 +36,17 @@ turn = deque([1, 2])
 
 # LOOP
 while True:
-    ## Display
+    # Display
     for count, item in enumerate(board.display(), 1):
         print(item, end="")
         if count % 3 == 0:
             print()
-    ## Ask for Input
+    # Display current player
+    print(f"Your are player {turn[0]}")
+    # Ask for Input
     while True:
         try:
-            ### Check the input
+            # Check the input
             prompt = list(map(int, input("Put in: ").split(" ")))
             if len(prompt) != 2:
                 print("Input needs to be exactly 2 long")
@@ -59,5 +61,10 @@ while True:
             print("Invalid input")
         else:
             break
-    ## Insert Input
+    # Insert Input
     board.insert(prompt, turn[0])
+    # TODO: Find if win state
+    # Find if win or full state
+    # End turn
+    turn.append(turn[0])
+    turn.popleft()
